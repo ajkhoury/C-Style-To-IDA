7d72422d7c7f6823fc41a9f36e4dfc32066f646c
dude719 <blood_099@hotmail.com>
2016-03-17 16:13:09 -0300

    Added files via upload

---
+++ c-style-to-ida.py
@@ -0,0 +1,25 @@
+import sys
+import re
+
+def get_input(prompt):
+    print prompt,
+    lines = []
+    line = raw_input()
+    while line != "":
+        line = line.replace(" ", "").replace("\t", "")
+        lines.append(line)
+        line = raw_input()
+    lines = ''.join(lines)
+    return lines
+
+def main():
+    byte_array_str = get_input("Enter C-Style byte array:")
+    if (byte_array_str):
+        byte_array_str = byte_array_str[2:]
+        byte_array_str = re.sub(',0x', ' ', byte_array_str)        
+        print("Converted byte array: " + byte_array_str)
+    else:
+        print("Error: invalid input")
+    
+if __name__ == "__main__":
+    main()
