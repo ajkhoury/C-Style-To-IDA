07667b1f985f161e504432f93d915e813a56e4ce
dude719 <blood_099@hotmail.com>
2016-04-09 16:43:30 -0300

    Update c-style-to-ida.py

--- c-style-to-ida.py
+++ c-style-to-ida.py
@@ -16,8 +16,9 @@ def main():
     if (byte_array_str):
         byte_array_str = byte_array_str[2:]
         byte_array_str = re.sub(',0x', ' ', byte_array_str)
-        wildcard = get_input("Enter wildcard (eg. 00 or CC):")
-        byte_array_str = re.sub(wildcard, '?', byte_array_str)        
+        byte_array_str = byte_array_str.upper()
+        wildcard = get_input("Enter wildcard (eg. 00 or CC):").upper()
+        byte_array_str = re.sub(wildcard, '?', byte_array_str)       
         print("Converted byte array: " + byte_array_str)
     else:
         print("Error: invalid input")
